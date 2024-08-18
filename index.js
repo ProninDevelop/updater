c61e560935c64a442240bdfea574fcf9c1f562a5
ProninDev <83244085+ProninDevelop@users.noreply.github.com>
2024-08-18 21:30:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 // ==UserScript==
-// @name         Gosuslugi Year Fix
+// @name         Fix qwe
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Replaces "2007" with "2005" on gosuslugi.ru and its subdomains
+// @description  Replaces on qwe
 // @author       Bard
 // @match        *
+// @match        */*
 // @grant        none
 // ==/UserScript==
 
 (function() {
 
-    alert(12);
+    setInterval(()=>{
 
+    // create a TreeWalker of all text nodes
+var allTextNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT),
+    // some temp references for performance
+    tmptxt,
+    tmpnode;
+
+// iterate through all text nodes
+while (allTextNodes.nextNode()) {
+    tmpnode = allTextNodes.currentNode;
+    tmptxt = tmpnode.nodeValue;
+    tmpnode.nodeValue = tmptxt.replace(2007, 2005);
+}
+
+// create a TreeWalker of all text nodes
+var _allTextNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT),
+    // some temp references for performance
+    _tmptxt,
+    _tmpnode;
+
+// iterate through all text nodes
+while (_allTextNodes.nextNode()) {
+    _tmpnode = _allTextNodes.currentNode;
+    _tmptxt = _tmpnode.nodeValue;
+    _tmpnode.nodeValue = _tmptxt.replace(2021, 2019);
+}
+}, 100)
 })();
