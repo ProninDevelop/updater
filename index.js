414dfde619668e66314fb935e8d9f429203e63f4
ProninDev <83244085+ProninDevelop@users.noreply.github.com>
2024-08-18 21:36:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,31 +12,6 @@
 (function() {
 
     setInterval(()=>{
-
-    // create a TreeWalker of all text nodes
-var allTextNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT),
-    // some temp references for performance
-    tmptxt,
-    tmpnode;
-
-// iterate through all text nodes
-while (allTextNodes.nextNode()) {
-    tmpnode = allTextNodes.currentNode;
-    tmptxt = tmpnode.nodeValue;
-    tmpnode.nodeValue = tmptxt.replace(2007, 2005);
-}
-
-// create a TreeWalker of all text nodes
-var _allTextNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT),
-    // some temp references for performance
-    _tmptxt,
-    _tmpnode;
-
-// iterate through all text nodes
-while (_allTextNodes.nextNode()) {
-    _tmpnode = _allTextNodes.currentNode;
-    _tmptxt = _tmpnode.nodeValue;
-    _tmpnode.nodeValue = _tmptxt.replace(2021, 2019);
-}
+        alert(12);
 }, 100)
 })();
