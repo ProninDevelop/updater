394d2bcd20b7dac502fab6108a2b2faccb4f971e
ProninDev <83244085+ProninDevelop@users.noreply.github.com>
2024-08-18 16:49:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,39 +11,7 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
 
     alert(12);
-    // create a TreeWalker of all text nodes
-var allTextNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT),
-    // some temp references for performance
-    tmptxt,
-    tmpnode,
-    // compile the RE and cache the replace string, for performance
-    cakeRE = /cake/g,
-    replaceValue = "pie";
-
-// iterate through all text nodes
-while (allTextNodes.nextNode()) {
-    tmpnode = allTextNodes.currentNode;
-    tmptxt = tmpnode.nodeValue;
-    tmpnode.nodeValue = tmptxt.replace(2007, 2005);
-}
-
-// create a TreeWalker of all text nodes
-var allTextNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT),
-    // some temp references for performance
-    tmptxt,
-    tmpnode,
-    // compile the RE and cache the replace string, for performance
-    cakeRE = /cake/g,
-    replaceValue = "pie";
-
-// iterate through all text nodes
-while (allTextNodes.nextNode()) {
-    tmpnode = allTextNodes.currentNode;
-    tmptxt = tmpnode.nodeValue;
-    tmpnode.nodeValue = tmptxt.replace(2021, 2019);
-}
 
 })();
